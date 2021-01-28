c85f695ea5625e5423fe8ae3030baaf3bda5c82f
Shehab Ahmed Khalil <shehabkhaleel@gmail.com>
2021-01-28 02:42:39 +0200

    enhanced-code

--- script.js
+++ script.js
@@ -1,122 +1,22 @@
 
 /*adding event listener to each flipping card, it flips horisontally showing whats on the other side and changes color*/
 let boxes=document.querySelectorAll('.box');
-let correct=[];
-for(i=0;i<16;i++){
-    correct.push(false)
+
+let icons=["brandico-facebook-rect","brandico-tumblr-rect","brandico-linkedin-rect","brandico-instagram","brandico-github","brandico-github-text","brandico-tumblr-rect","brandico-github-text","brandico-linkedin-rect","brandico-instagram","brandico-skype","brandico-twitter-bird","brandico-github","brandico-twitter-bird","brandico-skype","brandico-facebook-rect"]
+let divs=[];
+for(let i=0;i<=boxes.length-1;i++){
+    divs.push(boxes[i])
 }
-const one=document.getElementById('one')
-    one.addEventListener('click',function(ev){
-        one.innerHTML='<p class="brandico-facebook-rect"></p>'
-        one.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-        ev.preventDefault();
+
+for(let i=0;i<=boxes.length-1;i++){
+    boxes[i].addEventListener('click',function(ev){
+        if(ev.target.style.backgroundColor != "rgb(9, 255, 0)"){
+            ev.target.innerHTML=`<p class=${icons[i]}></p>`
+            ev.target.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
+        }
+            ev.preventDefault();
     })
-
-const sixteen=document.getElementById('sixteen')
-    sixteen.addEventListener('click',function(ev){
-        sixteen.innerHTML='<p class="brandico-facebook-rect"></p>'
-        sixteen.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-        ev.preventDefault();
-    })
-
-
-const two=document.getElementById('two')
-two.addEventListener('click',function(ev){
-    two.innerHTML='<p class="brandico-tumblr-rect"></p>'
-    two.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const seven=document.getElementById('seven')
-seven.addEventListener('click',function(ev){
-    seven.innerHTML='<p class="brandico-tumblr-rect"></p>'
-    seven.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const nine=document.getElementById('nine')
-nine.addEventListener('click',function(ev){
-    nine.innerHTML='<p class="brandico-linkedin-rect"></p>'
-    nine.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const three=document.getElementById('three')
-three.addEventListener('click',function(ev){
-    three.innerHTML='<p class="brandico-linkedin-rect"></p>'
-    three.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const four=document.getElementById('four')
-four.addEventListener('click',function(ev){
-    four.innerHTML='<p class="brandico-instagram"></p>'
-    four.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const ten=document.getElementById('ten')
-ten.addEventListener('click',function(ev){
-    ten.innerHTML='<p class="brandico-instagram"></p>'
-    ten.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const five=document.getElementById('five')
-five.addEventListener('click',function(ev){
-    five.innerHTML='<p class="brandico-github"></p>'
-    five.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const thirteen=document.getElementById('thirteen')
-thirteen.addEventListener('click',function(ev){
-    thirteen.innerHTML='<p class="brandico-github"></p>'
-    thirteen.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const six=document.getElementById('six')
-six.addEventListener('click',function(ev){
-    six.innerHTML='<p class="brandico-github-text"></p>'
-    six.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const eight=document.getElementById('eight')
-eight.addEventListener('click',function(ev){
-    eight.innerHTML='<p class="brandico-github-text"></p>'
-    eight.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const eleven=document.getElementById('eleven')
-eleven.addEventListener('click',function(ev){
-    eleven.innerHTML='<p class="brandico-skype"></p>'
-    eleven.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const fifteen=document.getElementById('fifteen')
-fifteen.addEventListener('click',function(ev){
-    fifteen.innerHTML='<p class="brandico-skype"></p>'
-    fifteen.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const twelve=document.getElementById('twelve')
-twelve.addEventListener('click',function(ev){
-    twelve.innerHTML='<p class="brandico-twitter-bird"></p>'
-    twelve.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
-
-const fourteen=document.getElementById('fourteen')
-fourteen.addEventListener('click',function(ev){
-    fourteen.innerHTML='<p class="brandico-twitter-bird"></p>'
-    fourteen.setAttribute('style','animation-name: click;animation-duration: 2s;background-color: rgb(78, 112, 163);transform: rotateY(360deg);transition: transform 0.8s')
-    ev.preventDefault();
-})
+}
 
 // declaring counter for clicks, array to store the clicked cards, array to store whats inside the clicked cards, and a nodeList of all cards
 let clickCount=0;
@@ -126,7 +26,8 @@ let targetArray=[];
 // starting a loop over the cards nodeList, and adding event listener to each one of them
 for(i=0;i<=boxes.length-1;i++){
         boxes[i].addEventListener('click',function(ev){
-            // keep pushing the clicked cards, whats inside them in the declared arrays, and count the clicks, if the number of clicks is less than two
+            if(ev.target.style.backgroundColor != "rgb(9, 255, 0)"){
+                // keep pushing the clicked cards, whats inside them in the declared arrays, and count the clicks, if the number of clicks is less than two
             if(clickCount<2){
                 clickCount++;
                 clickArray.push(ev.target.innerHTML);
@@ -168,6 +69,7 @@ for(i=0;i<=boxes.length-1;i++){
             }
             }
             ev.preventDefault();
+        }
         })
     }
 
